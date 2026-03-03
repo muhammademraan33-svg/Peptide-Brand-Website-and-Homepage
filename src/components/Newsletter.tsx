c1634a3113180cc74449ea6086386b13b65bc6c4
muhammadimrantrilogy <muhammad.imran@trilogy.com>
2026-03-03 14:51:04 +0500

    Initial fixed

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -1,4 +1,5 @@
-import { FormEvent, useState } from 'react'
+import type { FormEvent } from 'react'
+import { useState } from 'react'
 
 export function NewsletterSection() {
   const [submitted, setSubmitted] = useState(false)
